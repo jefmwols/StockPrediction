bb6f5a39047222d474acb37195323253b100c3b7
Jeff Wolski <jeffersonwolski@gmail.com>
2019-09-08 16:47:31 -0500

    implement quadratic regressions and plots

--- stock_regres.py
+++ stock_regres.py
@@ -30,7 +30,6 @@ y = df[y_key].values.reshape(-1,1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
-
 # Linear regression
 clfreg = LinearRegression(n_jobs=-1)
 clfreg.fit(X_train, y_train)
@@ -47,27 +46,25 @@ clfpoly2 = make_pipeline(PolynomialFeatures(2), Ridge())
 clfpoly2.fit(X_train, y_train)
 ridge2 = clfpoly2.named_steps['ridge']
 print("\n***\nQuadratic 2")
-
 print(ridge2.coef_)
+y_pred2 = clfpoly2.predict(X_test)
+df_t2 = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred2.flatten()})
+print(df_t2)
 
 # Quadratic Regression 3
 clfpoly3 = make_pipeline(PolynomialFeatures(3), Ridge())
 clfpoly3.fit(X_train, y_train)
 ridge3 = clfpoly2.named_steps['ridge']
 print("\n***\nQuadratic 3")
-
 print(ridge3.coef_)
+y_pred3 = clfpoly3.predict(X_test)
+df_t3 = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred3.flatten()})
+print(df_t3)
 
-# y_pred1 = clfreg.predict(X_test)
-# df_t = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred1.flatten()})
-# print(df_t)
 
-# df.plot(x=X_key, y=y_key, style='o')  
-# plt.title(X_key + 'vs' + y_key)  
-# plt.xlabel(X_key)  
-# plt.ylabel(y_key)  
-# plt.show()
 
 plt.scatter(X_test, y_test,  color='gray')
 plt.plot(X_test, y_pred1, color='red', linewidth=2)
+plt.plot(X_test, y_pred2, color='blue', linewidth=2)
+plt.plot(X_test, y_pred3, color='green', linewidth=2)
 plt.show()
